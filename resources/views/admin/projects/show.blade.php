a5e7ad50790e591b4eb669771671a6f394ca67b3
manuelgaleone <galeonemanuel14@gmail.com>
2023-01-10 18:12:12 +0100

    Fixed show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.admin')
 
 @section('index')
-@foreach ($projects as $project)
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800 py-4">{{$project->title}}</h1>
+<div class="text-center">
+    <div class="text-center mb-4">
+        <h1 class=" text-center mb-0 text-gray-800 py-4">{{$project->title}}</h1>
+    </div>
+    <img src="{{$project->thumb}}" alt="{{$project->title}}">
+    <p class="my-4">
+        {{$project->content}}
+    </p>
 </div>
-@endforeach
 @endsection
